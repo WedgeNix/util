cd14ef4d5812f0dca64f33ca635e238d552e299f
WedgeNix <wedgenix.acctdepartment@gmail.com>
2017-05-19 09:35:45 -0700

    Create util.go

---
+++ util.go
@@ -0,0 +1,10 @@
+package util
+
+// Load assumes to read the 
+func Load(name string, v interface{}) {
+	f, err := os.Open(name + ".json")
+	if err != nil {
+		fmt.Println(err)
+	}
+	json.NewDecoder(f).Decode(v)
+}
