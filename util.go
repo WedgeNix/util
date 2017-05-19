a75d32ba370734045a4d821b69438c765c65a4d8
PǝRV <alexander.w.matulionis@gmail.com>
2017-05-19 09:38:34 -0700

    Imports

--- util.go
+++ util.go
@@ -1,6 +1,12 @@
 package util
 
-// Load assumes to read the 
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+)
+
+// Load assumes to read the
 func Load(name string, v interface{}) {
 	f, err := os.Open(name + ".json")
 	if err != nil {
