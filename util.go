75fc480db93e969a3cd426b61e03e8a6ed1ab03c
PǝRV <alexander.w.matulionis@gmail.com>
2017-05-19 16:15:30 -0700

    Save function

--- util.go
+++ util.go
@@ -1,6 +1,7 @@
 package util
 
 import (
+	"bytes"
 	"encoding/base64"
 	"encoding/json"
 	"fmt"
@@ -20,38 +21,46 @@ type HTTPLogin struct {
 // Get receives an HTTP response from the given URL using authorization.
 func (lgn *HTTPLogin) Get(url string) *http.Response {
 	req, err := http.NewRequest(http.MethodGet, url, nil)
-	if err != nil {
-		fmt.Println(err)
-	}
+	E(err)
 	enc := base64.StdEncoding.EncodeToString([]byte(lgn.User + ":" + lgn.Pass))
 	req.Header.Add("Authorization", "Basic "+enc)
 	cl := http.Client{Timeout: 10 * time.Second}
 	resp, err := cl.Do(req)
-	if err != nil {
-		fmt.Println(err)
-	}
+	E(err)
 	return resp
 }
 
-// Load assumes to read the
+// Load reads in a JSON from either an HTTP GET response or a local file.
 func Load(name string, v interface{}) {
 	var r io.Reader
 	if strings.Contains(name, "https://") || strings.Contains(name, "http://") {
 		resp, err := http.Get(name)
-		if err != nil {
-			fmt.Println(err)
-		}
+		E(err)
 		r = resp.Body
 		defer resp.Body.Close()
 	} else {
 		f, err := os.Open(name + ".json")
-		if err != nil {
-			fmt.Println(err)
-		}
+		E(err)
 		r = f
 		defer f.Close()
 	}
 	json.NewDecoder(r).Decode(v)
+}
+
+// Save writes a JSON to either an HTTP POST response or a local file.
+func Save(name string, v interface{}) {
+	if strings.Contains(name, "https://") || strings.Contains(name, "http://") {
+		b, err := json.Marshal(v)
+		E(err)
+		r := bytes.NewReader(b)
+		_, err = http.Post(name, "application/json", r)
+		E(err)
+	} else {
+		f, err := os.Create(name + ".json")
+		E(err)
+		defer f.Close()
+		json.NewEncoder(f).Encode(v)
+	}
 }
 
 // E reports the error if there is any.
