db1d059779c9c8457ec7b712d40d9ab91dcdf083
PǝRV <alexander.w.matulionis@gmail.com>
2017-05-19 11:37:01 -0700

    Added Load's ability to pull from HTTP

--- util.go
+++ util.go
@@ -3,14 +3,27 @@ package util
 import (
 	"encoding/json"
 	"fmt"
+	"io"
+	"net/http"
 	"os"
+	"strings"
 )
 
 // Load assumes to read the
 func Load(name string, v interface{}) {
-	f, err := os.Open(name + ".json")
-	if err != nil {
-		fmt.Println(err)
+	var r io.Reader
+	if strings.Contains(name, "https://") || strings.Contains(name, "http://") {
+		res, err := http.Get(name)
+		if err != nil {
+			fmt.Println(err)
+		}
+		r = res.Body
+	} else {
+		f, err := os.Open(name + ".json")
+		if err != nil {
+			fmt.Println(err)
+		}
+		r = f
 	}
-	json.NewDecoder(f).Decode(v)
+	json.NewDecoder(r).Decode(v)
 }
