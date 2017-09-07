2620da299578cf7acbca26c0fbc7d86432ac3f9f
PǝRV <alexander.w.matulionis@gmail.com>
2017-09-07 09:47:10 -0700

    back 2 minutes

--- log.go
+++ log.go
@@ -40,13 +40,9 @@ var (
 // Log calls Output to print to the standard logger.
 // Arguments are handled in the manner of fmt.Println.
 func Log(v ...interface{}) {
-	fnm := LANow().Format("Mon Jan 2, 2006") + ".log"
+	fnm := LANow().Format("Mon Jan 2, 2006 (3∶04 PM)") + ".log"
 	if _, err := os.Stat(fnm); os.IsNotExist(err) {
 		f, _ = os.Create(fnm)
-		w = io.MultiWriter(os.Stdout, f)
-		ready <- true
-	} else if w == nil {
-		f, _ = os.Open(fnm)
 		w = io.MultiWriter(os.Stdout, f)
 		ready <- true
 	}
