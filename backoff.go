c9c3a1a47be0686b899f597a6d77142c40c3e11c
PǝRV <alexander.w.matulionis@gmail.com>
2017-10-25 14:33:38 -0700

    > 200

--- backoff.go
+++ backoff.go
@@ -3,45 +3,65 @@ package util
 import (
 	"math"
 	"math/rand"
-	"sync"
+	"net/http"
 	"time"
 )
 
-var (
-	BackoffTimeout = 48 * time.Second
+// var (
+// 	BackoffTimeout = 48 * time.Second
 
-	backoffLookupl sync.Mutex
-	backoffLookup  = map[interface{}]*backoffData{}
-)
+// 	backoffLookupl sync.Mutex
+// 	backoffLookup  = map[interface{}]*backoffData{}
+// )
 
-type backoffData struct {
-	sync.Mutex
-	wait  int
-	tries float64
+// type backoffData struct {
+// 	sync.Mutex
+// 	wait  int
+// 	tries float64
+// }
+
+type Backoff struct {
+	Attempt int
+	Timeout time.Duration
 }
 
-func Backoff(f func() (interface{}, error)) {
-	backoffLookupl.Lock()
-	data, found := backoffLookup[f]
-	if !found {
-		backoffLookup[f] = &backoffData{}
+func (b *Backoff) Wait(resp *http.Response, err error) (*http.Response, error) {
+	if b.Timeout == 0 {
+		b.Timeout = 64000
+	} else if b.Timeout < 32000 {
+		panic("backoff timeout too low; 32s minimum")
 	}
-	backoffLookupl.Unlock()
-
-	maxWait := int(BackoffTimeout.Seconds() * 1000)
-
-	var res interface{}
-	var err error
-
-	data.Lock()
-	for ; data.wait < maxWait && res == nil; data.tries++ {
-		res, err = f()
-		if err != nil {
-			data.wait = int(math.Min(float64(maxWait), math.Pow(2, data.tries)+float64(rand.Intn(1000))+1))
-			time.Sleep(time.Duration(data.wait) * time.Millisecond)
-		}
+	maxWait := b.Timeout.Seconds() * 1000
+	wait := int(math.Min(maxWait, math.Pow(2, float64(b.Attempt))+float64(rand.Intn(1000))+1))
+	if err != nil || resp.StatusCode > 200 {
+		time.Sleep(time.Duration(wait) * time.Millisecond)
 	}
-	data.Unlock()
-
-	println("util.go: backoff timed out")
+	b.Attempt++
+	return resp, err
 }
+
+// func Backoff(f func() (interface{}, error)) {
+// 	backoffLookupl.Lock()
+// 	data, found := backoffLookup[f]
+// 	if !found {
+// 		backoffLookup[f] = &backoffData{}
+// 	}
+// 	backoffLookupl.Unlock()
+
+// 	maxWait := int(BackoffTimeout.Seconds() * 1000)
+
+// 	var res interface{}
+// 	var err error
+
+// 	data.Lock()
+// 	for ; data.wait < maxWait && res == nil; data.tries++ {
+// 		res, err = f()
+// 		if err != nil {
+// 			data.wait = int(math.Min(float64(maxWait), math.Pow(2, data.tries)+float64(rand.Intn(1000))+1))
+// 			time.Sleep(time.Duration(data.wait) * time.Millisecond)
+// 		}
+// 	}
+// 	data.Unlock()
+
+// 	println("util.go: backoff timed out")
+// }
